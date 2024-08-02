91ab25065049e9e29888183e7b5c946bd56e5f73
Daniel Work <daniel.work@aemo.com.au>
2024-08-02 12:53:55 +1000

    SELECT DISTINCT OWNER AS name FROM DBA_OBJECTS

--- DatabaseSchemaReader/ProviderSchemaReaders/Databases/Oracle/Schemas.cs
+++ DatabaseSchemaReader/ProviderSchemaReaders/Databases/Oracle/Schemas.cs
@@ -10,7 +10,11 @@ namespace DatabaseSchemaReader.ProviderSchemaReaders.Databases.Oracle
     {
         public Schemas(int? commandTimeout) : base(commandTimeout, null)
         {
-            Sql = @"SELECT USERNAME AS name FROM ALL_USERS ORDER BY USERNAME";
+            //Sql = @"SELECT USERNAME AS name FROM ALL_USERS ORDER BY USERNAME";          //Returns all users reguardless of if they are actual owners of database objects         
+            //Sql = @"SELECT DISTINCT OWNER AS name FROM ALL_OBJECTS ORDER BY OWNER";     //Returns distinct list of all owners of database objects
+            Sql = @"SELECT DISTINCT OWNER AS name FROM DBA_OBJECTS ORDER BY OWNER";     //Returns distinct list of all owners of database objects but about 20x faster than using ALL_OBJECTS (which makes sense when you compare the DDLs of the views)
+            //The other difference between ALL_OBJECTS and DBA_OBJECTS is that ALL_OBJECTS has builtin permissions checking to only show the objects that the current connecting user has permissions to see
+            //See: https://asktom.oracle.com/ords/f?p=100:11:0::::P11_QUESTION_ID:9287207731148
         }
 
         protected override void AddParameters(DbCommand command)
